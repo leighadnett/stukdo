039a94de23b86ac3f98159f79a7fc5ae97ceeb5a
Leigh Adnett <leigh@leighadnett.com>
2015-06-01 23:32:47 +0200

    Added link detection

--- stukdo/db/schema.rb
+++ stukdo/db/schema.rb
@@ -11,14 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150531075145) do
+ActiveRecord::Schema.define(version: 20150601212556) do
 
   create_table "tasks", force: :cascade do |t|
     t.text     "content"
-    t.datetime "created_at",                   null: false
-    t.datetime "updated_at",                   null: false
-    t.string   "state",      default: "to_do"
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.string   "state",        default: "to_do"
     t.integer  "user_id"
+    t.text     "content_html"
   end
 
   create_table "users", force: :cascade do |t|
